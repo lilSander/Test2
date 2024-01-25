b41ba874f6158702ed9f686d97aa1db53eb40c89
Lysander <73167687+lilSander@users.noreply.github.com>
2024-01-25 10:41:19 +0100

    new file added

---
+++ Sprache.java
@@ -0,0 +1,12 @@
+
+//The Ship of Theseus is a thought experiment about whether an object, after having had all of its original
+ components replaced, remains the same object.
+
+In Greek mythology, Theseus, mythical king and founder of the city Athens, rescued the children of Athens from 
+King Minos after slaying the minotaur and then escaped onto a ship going to Delos. Each year, the Athenians would 
+commemorate this by taking the ship on a pilgrimage to Delos to honour Apollo. A question was raised by ancient philosophers: 
+After several hundreds of years of maintenance, if each individual part of the Ship of Theseus was replaced, one after the other, was it still the same ship?
+
+In contemporary philosophy, this thought experiment has applications to the philosophical study of identity over time, 
+and has inspired a variety of proposed solutions and concepts in contemporary philosophy of mind concerned with the persistence of personal identity. 
+
